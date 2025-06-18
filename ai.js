bb360caef01986e328246b94e54ed02e7237df18
25LP9810 25LP9810 <25LP9810@admins-iMac.local>
2025-06-18 13:28:42 +0800

    aiiiiiiiiiii

---
+++ ai.js
@@ -0,0 +1,14 @@
+import { GoogleGenAI } from "@google/genai";
+
+const apiKey = "AIzaSyBU-D0IxwngZUyW5fGW-qjLEBHoQNtaBgo";
+const ai = new GoogleGenAI({ apiKey: apiKey });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
